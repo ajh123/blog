78893b7420b73e842c8aa98b4dc5f29c36618158
Samuel Hulme <41990982+ajh123@users.noreply.github.com>
2025-09-14 18:49:41 +0100

    feat: integrate PostHog for analytics tracking
    - Updated package.json to downgrade @astrojs/vercel and add posthog-js and posthog-node dependencies.
    - Removed PostHog component from the project as it is now handled through a new integration.
    - Modified PageLayout to remove PostHog component usage.
    - Added a new posthog.ts file to manage PostHog integration for both client and server.
    - Updated index.astro to include necessary changes for PostHog integration.
    - Ensured style.css has a newline at the end of the file for consistency.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,17 @@ import vercel from '@astrojs/vercel';
 
 import preact from '@astrojs/preact';
 
+import posthog from './src/lib/posthog';
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [preact()]
+  integrations: [
+    preact(),
+    posthog('phc_n17zLe4q3Ejrry0lUNzOti6FzeDBVuevjuL91eoxpSa', {
+      api_host: 'https://eu.i.posthog.com',
+      defaults: '2025-05-24'
+    })
+  ]
 });
